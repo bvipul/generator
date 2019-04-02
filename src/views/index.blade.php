791a38623475cb2ef320be84ce2fa7779cd9a655
Viral Solani <viral.solani@gmail.com>
2019-04-02 12:35:56 +0530

    Resolve csrf token issues for ajax request

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -68,9 +68,14 @@
 @section('after-scripts')
     {{-- For DataTables --}}
     {{ Html::script(mix('js/dataTable.js')) }}
-    
+
     <script>
         $(function() {
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
             var dataTable = $('#modules-table').dataTable({
                 processing: true,
                 serverSide: true,
